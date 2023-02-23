0ede0e89a98d6a9b5eb5cad86fbe3cdf3a6cbfee
Flora704 <ghlee704@uw.edu>
2023-02-23 00:08:14 -0800

    5 values done

--- Flora summary.R
+++ Flora summary.R
@@ -6,28 +6,33 @@ library("ggplot2")
 
 spl_df <- read.csv("~/Checkout_df.csv", stringsAsFactors = F)
 
-# What is the average number of checkouts for this dataset related to George Orwell?
+# What is the average number of checkouts in this dataset?
 avg_num_checkout <- spl_df %>% summarize(mean_checkout = mean(Checkouts)) %>% pull(mean_checkout)
 
 avg_num_checkout
 
-# What is the year with the most/least checkouts for a book that you're interested in?
-min_checkout <- spl_df %>% filter(Checkouts == min(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
+# What is the most checkouts in 2020 in this dataset?
 
-min_checkout 
+max_checkout_2020 <- spl_df %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
 
-max_checkout <- spl_df %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
+max_checkout_2020
 
-max_checkout 
+# What is the title for the most checkouts in 2020 in this dataset?
 
-# What is the year with the most/least checkouts for ebooks?
-max_ebook <- spl_df %>% group_by(MaterialType == "EBOOK") %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
+max_checkout_2020_title <- spl_df %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Title)
 
-max_ebook 
+max_checkout_2020_title
 
-min_ebook <- spl_df %>% group_by(MaterialType == "EBOOK") %>% filter(Checkouts == min(Checkouts, na.rm = TRUE)) %>% pull(CheckoutYear)
 
-min_ebook
+# What is the most checkouts in 2020 for ebooks in this dataset?
+
+max_ebook_2020 <- spl_df %>% filter(MaterialType == "EBOOK") %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
+
+max_ebook_2020
+
+max_ebook_2020_title <- spl_df %>% filter(MaterialType == "EBOOK") %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Title)
+
+max_ebook_2020_title
 
 # How has the number of print book checkouts changed over time? Compare the total number of checkouts now and five years ago. 
 total_checkout_2023 <- spl_df %>% filter(CheckoutYear == 2023) %>% summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  pull(total)
@@ -44,15 +49,65 @@ Difference_between_2023_and_2018
 
 # How many NEW checkouts were there when new checkouts occurred the most? 
 
-
 most_new_checkouts <- spl_df %>% mutate(new_Checkouts = Checkouts - lag(Checkouts)) %>% filter(new_Checkouts == max(new_Checkouts, na.rm = TRUE)) %>% pull(new_Checkouts)
 
 most_new_checkouts
 
 
 
-### First Trends Over Time Chart
 
+
+get_year_materialtype_checkouts <- function() {
+  df <- spl_df %>% 
+    select(MaterialType, CheckoutYear, Checkouts) %>%
+    filter(MaterialType == "EBOOK") %>%
+    group_by(CheckoutYear) %>%
+    summarize(total_Checkouts = sum(Checkouts)) %>%
+    select(CheckoutYear, total_Checkouts)
+  return(df)
+}
+
+plot_chart1 <- function()  {
+  df <- get_year_materialtype_checkouts()
+  c <- ggplot(df) +
+    geom_col(mapping = aes(x = CheckoutYear, y = total_Checkouts))
+  return(c)
+}
+
+chart_1 <- plot_chart1()
+chart_1
+
+
+
+
+
+
+
+
+
+
+
+### First Trends Over Time Chart
+ # How has the number of print book checkouts changed over time? Compare the total number of checkouts now and five years ago. 
+total_checkout_2023 <- spl_df %>% filter(CheckoutYear == 2023) %>% summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  pull(total)
+
+total_checkout_2023
+
+total_checkout_2018 <- spl_df %>% filter(CheckoutYear == 2018) %>% summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  pull(total)
+
+total_checkout_2018
+
+Difference_between_2023_and_2018 <- total_checkout_2018 - total_checkout_2023
+
+Difference_between_2023_and_2018
+
+bp <- ggplot(data = spl_df) +
+  geom_col(mapping = aes( x = ,
+                          y = charges,
+                          fill = sex)) 
+
+bp + scale_fill_manual(values = c("#FFFEA6", "#6AC2FF"))+                    
+  scale_y_continuous(labels = label_number_si()
 
 
 
