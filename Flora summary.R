2efc725b979161a36ecf758acc2d69e4ab4c3751
Flora704 <ghlee704@uw.edu>
2023-02-25 01:30:06 -0800

    change

--- Flora summary.R
+++ Flora summary.R
@@ -8,37 +8,59 @@ library("scales")
 spl_df <- read.csv("~/Checkout_df.csv", stringsAsFactors = F)
 
 # What is the average number of checkouts in this dataset?
-avg_num_checkout <- spl_df %>% summarize(mean_checkout = mean(Checkouts)) %>% pull(mean_checkout)
+avg_num_checkout <- spl_df %>% 
+  summarize(mean_checkout = mean(Checkouts)) %>% 
+  pull(mean_checkout)
 
 avg_num_checkout
 
 # What is the most checkouts in 2020 in this dataset?
-max_checkout_2020 <- spl_df %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
+max_checkout_2020 <- spl_df %>% 
+  filter(CheckoutYear == 2020) %>% 
+  filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% 
+  pull(Checkouts)
 
 max_checkout_2020
 
 # What is the title of the most checkouts in 2020 in this dataset? Pull the title.
-max_checkout_2020_title <- spl_df %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Title)
+max_checkout_2020_title <- spl_df %>% 
+  filter(CheckoutYear == 2020) %>% 
+  filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>%
+  pull(Title)
 
 max_checkout_2020_title
 
 
 # What is the most checkouts in 2020 for "ebook" in this dataset?
-max_ebook_2020 <- spl_df %>% filter(MaterialType == "EBOOK") %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Checkouts)
+max_ebook_2020 <- spl_df %>% 
+  filter(MaterialType == "EBOOK") %>% 
+  filter(CheckoutYear == 2020) %>% 
+  filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% 
+  pull(Checkouts)
 
 max_ebook_2020
 
 # What is the title of the most checkouts in 2020 for "ebook" in this dataset? Pull the title.
-max_ebook_2020_title <- spl_df %>% filter(MaterialType == "EBOOK") %>% filter(CheckoutYear == 2020) %>% filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% pull(Title)
+max_ebook_2020_title <- spl_df %>% 
+  filter(MaterialType == "EBOOK") %>% 
+  filter(CheckoutYear == 2020) %>% 
+  filter(Checkouts == max(Checkouts, na.rm = TRUE)) %>% 
+  pull(Title)
 
 max_ebook_2020_title
 
 # How has the number of print book checkouts changed over time? Compare the total number of checkouts now and five years ago. 
-total_checkout_2023 <- spl_df %>% filter(CheckoutYear == 2023) %>% summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  pull(total)
+total_checkout_2023 <- spl_df %>% 
+  filter(CheckoutYear == 2023) %>% 
+  summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  
+  pull(total)
 
 total_checkout_2023
 
-total_checkout_2018 <- spl_df %>% filter(CheckoutYear == 2018) %>% summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  pull(total)
+total_checkout_2018 <- spl_df %>% 
+  filter(CheckoutYear == 2018) %>% 
+  summarize(total = sum(Checkouts, na.rm = TRUE)) %>%  
+  pull(total)
 
 total_checkout_2018
 
@@ -47,7 +69,10 @@ Difference_between_2023_and_2018 <- total_checkout_2018 - total_checkout_2023
 Difference_between_2023_and_2018
 
 # How many NEW checkouts were there when new checkouts occurred the most? 
-most_new_checkouts <- spl_df %>% mutate(new_Checkouts = Checkouts - lag(Checkouts)) %>% filter(new_Checkouts == max(new_Checkouts, na.rm = TRUE)) %>% pull(new_Checkouts)
+most_new_checkouts <- spl_df %>% 
+  mutate(new_Checkouts = Checkouts - lag(Checkouts)) %>% 
+  filter(new_Checkouts == max(new_Checkouts, na.rm = TRUE)) %>% 
+  pull(new_Checkouts)
 
 most_new_checkouts
 
@@ -105,6 +130,16 @@ bp_sec <- ggplot(data = Second_df) +
 bp_sec
 
 ### Third Chart
+
+
+
+
+
+
+
+
+
+
 
 
 
